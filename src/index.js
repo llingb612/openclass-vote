01ab0dc4305eb2131912dd795f2a0f7bcf1911cb
llingb612 <s870089@gmail.com>
2025-08-02 21:11:06 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import VoteGrid from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <VoteGrid />
+  </React.StrictMode>
+);
